08e59e8621f2fc8e3be39cbce7d11a3c92ac3c6b
JohanThePawn <jjalarcon78@gmail.com>
2022-04-01 19:52:44 +0200

    3a subida

--- env.js
+++ env.js
@@ -104,7 +104,7 @@ var
 
   ////////uncomment the following line when downloading the app
 
-  //apiUrl = 'https://js.arcgis.com/3.39';
+  apiUrl = 'https://js.arcgis.com/3.39';
 
   //////////////////////////////////////////////////////////////
   allCookies = getAllCookies();
